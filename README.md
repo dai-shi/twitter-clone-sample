7d68bed8fe393e4ade0862265e51c111fd953257
daishi <daishi@axlight.com>
2013-10-23 22:01:50 +0900

    combined request to avoid many requests

--- README.md
+++ README.md
@@ -8,9 +8,15 @@ is usable.
 The original code: 
 [20130804_recorded](https://github.com/dai-shi/twitter-clone-sample/tree/20130804_recorded)
 is exactly the same as
-[screencast](http://dai-shi.github.io/social-cms-backend/ttyplay.html).
+[screencast](http://dai-shi.github.io/social-cms-backend/ttyplay.html)
+result.
 
 The running code:
 [20130920_openshift](https://github.com/dai-shi/twitter-clone-sample/tree/20130920_openshift)
 can be tried at
-[OpenShift](http://twitterclonesample-nodeangularapp.rhcloud.com/).
+[OpenShift server](http://twitterclonesample-nodeangularapp.rhcloud.com/).
+
+changes
+-------
+
+* use combined request to avoid many requests for getting likecount for each post.
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,32 +13,62 @@ factory('Like', function($resource) {
 }).
 
 controller('MyController', function($scope, User, Post, Like) {
-  $scope.myself = User.get({id: 'myself'});
+  $scope.myself = User.get({
+    id: 'myself'
+  });
   $scope.userinfo = {};
   $scope.ensureUserinfo = function(user_id) {
     if ($scope.userinfo[user_id]) return;
-    $scope.userinfo[user_id] = User.get({id: user_id});
+    $scope.userinfo[user_id] = User.get({
+      id: user_id
+    });
   };
 
-  $scope.posts = Post.query();
+  $scope.likecount = {};
+
+  Post.query(function(data) {
+    $scope.posts = data;
+
+    var queries = _.map(data, function(post) {
+      return {
+        post_id: post._id
+      };
+    });
+    Like.query({
+      id: 'count',
+      query: JSON.stringify(queries)
+    }, function(results) {
+      for (var i = 0; i < data.length; i++) {
+        $scope.likecount[data[i]._id] = results[i];
+      }
+    });
+  });
+
   $scope.postNewPost = function() {
     if (!$scope.newMessage) return;
-    Post.save({message: $scope.newMessage}, function() {
+    Post.save({
+      message: $scope.newMessage
+    }, function() {
       $scope.posts = Post.query();
     });
     $scope.newMessage = null;
   };
 
-  $scope.likecount = {};
-  $scope.ensureLikecount = function(post_id, refresh) {
-    if ($scope.likecount[post_id] && !refresh) return;
-    $scope.likecount[post_id] = Like.get({id: 'count',
-      query: JSON.stringify({post_id: post_id})});
-  };
   $scope.addLikecount = function(post_id) {
-    Like.save({post_id: post_id}, function() {
-      $scope.ensureLikecount(post_id, true);
+    Like.save({
+      post_id: post_id
+    }, function() {
       $scope.likecount[post_id].count = '...';
+      Like.get({
+        id: 'count',
+        query: JSON.stringify({
+          post_id: post_id
+        })
+      },
+
+      function(data) {
+        $scope.likecount[post_id] = data;
+      });
     });
   };
 
